2110f43ef0559bdf9374a011eb4a28c18b00f197
jordick <jord.pinom@alumnos.duoc.cl>
2019-09-25 15:28:49 -0300

    Ejercicio python

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+#rut, nombre, comuna,activo/inactivo
+#buscar por rut
+#comunas: concepción, talcahuano, hualpen
+
+while True:
+    print('''1. Ingresar cliente
+    2. Modificar cliente
+    3. Eliminar cliente
+    4. Buscar cliente
+    5. Contar clientes inactivos
+    6. Contar clientes por comuna''')
+    
